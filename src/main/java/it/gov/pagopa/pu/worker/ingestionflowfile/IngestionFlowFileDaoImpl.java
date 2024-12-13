4e8ab656007b70a5b5f4139ed4c14e7f3d133407
macacia <marco.cacia@eng.it>
2024-12-13 13:46:03 +0100

    P4ADEV-1653 removed service layer

--- src/main/java/it/gov/pagopa/pu/worker/ingestionflowfile/IngestionFlowFileDaoImpl.java
+++ src/main/java/it/gov/pagopa/pu/worker/ingestionflowfile/IngestionFlowFileDaoImpl.java
@@ -2,13 +2,24 @@ package it.gov.pagopa.pu.worker.ingestionflowfile;
 
 import it.gov.pagopa.payhub.activities.dao.IngestionFlowFileDao;
 import it.gov.pagopa.payhub.activities.dto.IngestionFlowFileDTO;
+import it.gov.pagopa.pu.worker.ingestionflowfile.mapper.IngestionFlowFileMapper;
+import it.gov.pagopa.pu.worker.ingestionflowfile.repository.IngestionFlowFileRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 public class IngestionFlowFileDaoImpl implements IngestionFlowFileDao {
-  @Override
+  private final IngestionFlowFileRepository repository;
+  private final IngestionFlowFileMapper ingestionFlowFileMapper;
+
+	public IngestionFlowFileDaoImpl(IngestionFlowFileRepository repository,
+                                  IngestionFlowFileMapper ingestionFlowFileMapper) {
+		this.repository = repository;
+		this.ingestionFlowFileMapper = ingestionFlowFileMapper;
+	}
+
+	@Override
   public Optional<IngestionFlowFileDTO> findById(Long ingestionFlowFileId) {
     return Optional.empty();
   }
