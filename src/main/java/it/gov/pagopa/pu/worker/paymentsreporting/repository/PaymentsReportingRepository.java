b55f9cdbc9defbfd582f7998dd9d45b48d6cec41
macacia <marco.cacia@eng.it>
2025-01-21 11:20:01 +0100

    P4ADEV-1579 removing no longer useful packages

--- src/main/java/it/gov/pagopa/pu/worker/paymentsreporting/repository/PaymentsReportingRepository.java
+++
@@ -1,9 +0,0 @@
-package it.gov.pagopa.pu.worker.paymentsreporting.repository;
-
-import it.gov.pagopa.pu.worker.paymentsreporting.model.PaymentsReporting;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentsReportingRepository extends JpaRepository<PaymentsReporting, Long> {
-}
