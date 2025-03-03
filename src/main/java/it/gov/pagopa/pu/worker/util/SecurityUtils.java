eb7d807647f2b4099bb9d48669574801104ba8f7
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2025-03-03 14:46:37 +0100

    fix: PII in REST_INVOKE logs (#38)

--- src/main/java/it/gov/pagopa/pu/worker/util/SecurityUtils.java
+++ src/main/java/it/gov/pagopa/pu/worker/util/SecurityUtils.java
@@ -1,8 +1,11 @@
 package it.gov.pagopa.pu.worker.util;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 
+import java.net.URI;
+import java.security.Principal;
 import java.util.Optional;
 
 public class SecurityUtils {
@@ -14,5 +17,22 @@ public class SecurityUtils {
       .flatMap(c -> Optional.ofNullable(c.getAuthentication()))
       .map(a -> ((Jwt) a.getCredentials()).getTokenValue())
       .orElse(null);
+  }
+
+  public static String getCurrentUserExternalId(){
+    return getAuthentication()
+      .map(Principal::getName)
+      .orElse(null);
+  }
+
+  private static Optional<Authentication> getAuthentication() {
+    return Optional.ofNullable(SecurityContextHolder.getContext())
+      .flatMap(c -> Optional.ofNullable(c.getAuthentication()));
+  }
+
+  public static String removePiiFromURI(URI uri){
+    return uri != null
+      ? uri.toString().replaceAll("=[^&]*", "=***")
+      : null;
   }
 }
--- src/test/java/it/gov/pagopa/pu/worker/util/SecurityUtilsTest.java
+++ src/test/java/it/gov/pagopa/pu/worker/util/SecurityUtilsTest.java
@@ -3,10 +3,13 @@ package it.gov.pagopa.pu.worker.util;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+
+import java.net.URI;
 
 class SecurityUtilsTest {
 
@@ -44,4 +47,28 @@ class SecurityUtilsTest {
     Assertions.assertSame(jwt, result);
   }
 //endregion
+
+  @Test
+  void givenJwtWhenGetCurrentUserExternalIdThenReturnPrincipalName(){
+    // Given
+    String principalName = "PRINCIPALNAME";
+    SecurityContextHolder.setContext(new SecurityContextImpl(new JwtAuthenticationToken(Mockito.mock(Jwt.class), null, principalName)));
+
+    // When
+    String result = SecurityUtils.getCurrentUserExternalId();
+
+    // Then
+    Assertions.assertSame(principalName, result);
+  }
+
+  @Test
+  void givenUriWhenRemovePiiFromURIThenOk(){
+    String result = SecurityUtils.removePiiFromURI(URI.create("https://host/path?param1=PII&param2=noPII"));
+    Assertions.assertEquals("https://host/path?param1=***&param2=***", result);
+  }
+
+  @Test
+  void givenNullUriWhenRemovePiiFromURIThenOk(){
+    Assertions.assertNull(SecurityUtils.removePiiFromURI(null));
+  }
 }
