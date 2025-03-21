f16cd9304b7dfe35e3e055784cc97cbafe7d0111
antonio.torre <antonio.torre@pagopa.it>
2025-03-21 11:49:14 +0100

    fix: aligning ObjectMapper used by Temporal with our

---
+++ src/main/java/it/gov/pagopa/pu/worker/config/TemporalConfig.java
@@ -0,0 +1,17 @@
+package it.gov.pagopa.pu.worker.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.temporal.common.converter.DataConverter;
+import io.temporal.common.converter.DefaultDataConverter;
+import io.temporal.common.converter.JacksonJsonPayloadConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TemporalConfig {
+  @Bean
+  public DataConverter temporalDataConverter(ObjectMapper objectMapper){
+    return DefaultDataConverter.newDefaultInstance()
+      .withPayloadConverterOverrides(new JacksonJsonPayloadConverter(objectMapper));
+  }
+}
