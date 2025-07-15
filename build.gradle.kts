e94879715f53a71152c775c30db9a2af0f8654f8
domenicogi <domenico.giacobbe@eng.it>
2025-07-15 13:50:06 +0200

    feat: P4ADEV-0001 update activity version (#143)

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,7 +46,7 @@ val temporalVersion = "1.29.0"
 val protobufJavaVersion = "4.31.1"
 val guavaVersion = "33.4.8-jre"
 
-val p4paActivitiesVersion = "1.134.3"
+val p4paActivitiesVersion = "1.135.1"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter")
