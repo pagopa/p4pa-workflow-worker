db62ef4146803d4479fe0d6c8cbdf3db2a18cc6c
michelemazzini <113985823+michelemazzini@users.noreply.github.com>
2025-10-21 16:56:58 +0200

    fix: P4ADEV-3861 fix activities version (#222)

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,7 +51,7 @@ val grpcBomVersion = "1.75.0"
 val guavaVersion = "33.5.0-jre"
 val commonsLang3Version = "3.19.0"
 
-val p4paActivitiesVersion = "1.154.15"
+val p4paActivitiesVersion = "1.158.0"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter")
