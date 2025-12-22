018832d83cc5a877af303025570d0d0f93088407
antonio.torre <antonio.torre@pagopa.it>
2025-12-22 10:55:23 +0100

    fix: Fix CVE, upgrade SpringBoot 4

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,7 +65,7 @@ val grpcBomVersion = "1.77.0"
 val guavaVersion = "33.5.0-jre"
 val commonsLang3Version = "3.20.0"
 
-val p4paActivitiesVersion = "P4ADEV-4309-SNAPSHOT" // TODO replace with release version when available
+val p4paActivitiesVersion = "1.163.1"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webmvc")
