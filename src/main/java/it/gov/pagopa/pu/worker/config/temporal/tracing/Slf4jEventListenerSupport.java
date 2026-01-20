496f3bddc008782588e90b942648d430a371d095
antonio.torre <antonio.torre@pagopa.it>
2026-01-20 16:33:13 +0100

    fix: Fix Temporal traceId occasionally empty

--- src/main/java/it/gov/pagopa/pu/worker/config/temporal/tracing/Slf4jEventListenerSupport.java
+++ src/main/java/it/gov/pagopa/pu/worker/config/temporal/tracing/Slf4jEventListenerSupport.java
@@ -3,6 +3,7 @@ package it.gov.pagopa.pu.worker.config.temporal.tracing;
 import io.micrometer.tracing.otel.bridge.EventListener;
 import io.micrometer.tracing.otel.bridge.EventPublishingContextWrapper;
 import io.opentelemetry.opentracingshim.SpanShimHolder;
+import io.opentracing.Span;
 import io.opentracing.SpanContext;
 import org.slf4j.MDC;
 import org.springframework.boot.micrometer.tracing.autoconfigure.TracingProperties;
@@ -37,14 +38,17 @@ public class Slf4jEventListenerSupport implements EventListener {
   }
 
   private void setContext() {
-    SpanContext currentSpanContext = SpanShimHolder.getCurrentSpan().context();
-    MDC.put("traceId", currentSpanContext.toTraceId());
-    MDC.put("spanId", currentSpanContext.toSpanId());
+    Span currentSpan = SpanShimHolder.getCurrentSpan();
+    if(currentSpan!=null) {
+      SpanContext currentSpanContext = currentSpan.context();
+      MDC.put("traceId", currentSpanContext.toTraceId());
+      MDC.put("spanId", currentSpanContext.toSpanId());
 
-    currentSpanContext.baggageItems().forEach(i -> {
-      if(correlationFields.contains(i.getKey())){
-        MDC.put(i.getKey(), i.getValue());
-      }
-    });
+      currentSpanContext.baggageItems().forEach(i -> {
+        if (correlationFields.contains(i.getKey())) {
+          MDC.put(i.getKey(), i.getValue());
+        }
+      });
+    }
   }
 }
