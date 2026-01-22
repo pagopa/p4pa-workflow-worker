fec408abec05f30e6316b74978b17b9aa1fc098a
Leonardo Pierangelini <leonardo.pierangelini@nttdata.com>
2026-01-22 10:54:33 +0100

    P4ADEV-4297: updated activities version to 1.168.3-SNAPSHOT.

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,7 +67,7 @@ val grpcBomVersion = "1.77.0"
 val guavaVersion = "33.5.0-jre"
 val commonsLang3Version = "3.20.0"
 
-val p4paActivitiesVersion = "1.168.0"
+val p4paActivitiesVersion = "1.168.3-SNAPSHOT"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webmvc")
