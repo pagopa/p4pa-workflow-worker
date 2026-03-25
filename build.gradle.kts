9eedb0bb938add2b581b7750862495d45b331037
michele.mazzini <michele.mazzini1996@gmail.com>
2026-03-25 11:22:27 +0100

    fix: temp version

--- build.gradle.kts
+++ build.gradle.kts
@@ -71,7 +71,7 @@ val commonsLang3Version = "3.20.0"
 val jackson2CoreVersion = "2.21.1"
 val jackson3CoreVersion = "3.1.0"
 
-val p4paActivitiesVersion = "1.183.1-SNAPSHOT"
+val p4paActivitiesVersion = "1.183.2-SNAPSHOT"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webmvc")
