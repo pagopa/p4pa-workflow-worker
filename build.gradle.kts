9eceade84bb4a7d0bad9472b21faeead09314777
MScarsella <marco.scarsella@pagopa.it>
2026-05-04 16:48:04 +0200

    fix: P4ADEV-4653 update activity version (#344)

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,7 +67,7 @@ val grpcBomVersion = "1.80.0"
 val guavaVersion = "33.6.0-jre"
 val commonsLang3Version = "3.20.0"
 
-val p4paActivitiesVersion = "1.191.6"
+val p4paActivitiesVersion = "1.192.0"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webmvc")
