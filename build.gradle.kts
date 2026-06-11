13157abadc3d03ff1886cbb54fa54c0401c7fae1
antonio.torre <antonio.torre@pagopa.it>
2026-06-11 12:02:18 +0200

    fix: Handling the possibility that a spontaneous form doesn't exist on the CSV

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,7 +67,7 @@ val grpcBomVersion = "1.80.0"
 val guavaVersion = "33.6.0-jre"
 val commonsLang3Version = "3.20.0"
 
-val p4paActivitiesVersion = "1.196.1"
+val p4paActivitiesVersion = "1.196.2"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webmvc")
