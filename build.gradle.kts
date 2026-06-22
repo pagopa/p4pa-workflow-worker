53a4f033d3ccea7d32079e85d2c81c9366a41038
antonio.torre <antonio.torre@pagopa.it>
2026-06-22 17:25:15 +0200

    fix: Fix CVE

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,14 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
   java
-  id("org.springframework.boot") version "4.0.6"
+  id("org.springframework.boot") version "4.1.0"
   id("io.spring.dependency-management") version "1.1.7"
   jacoco
-  id("org.sonarqube") version "7.2.3.7755"
+  id("org.sonarqube") version "7.3.1.8318"
   id("com.github.ben-manes.versions") version "0.54.0"
-  id("org.openapi.generator") version "7.21.0"
-  id("com.gorylenko.gradle-git-properties") version "2.5.7"
-  id("com.github.jk1.dependency-license-report") version "3.1.2"
+  id("org.openapi.generator") version "7.23.0"
+  id("com.gorylenko.gradle-git-properties") version "4.0.1"
+  id("com.github.jk1.dependency-license-report") version "3.1.4"
 }
 
 group = "it.gov.pagopa.payhub"
@@ -58,16 +58,18 @@ repositories {
 
 val springDocOpenApiVersion = "3.0.3"
 val openApiToolsVersion = "0.2.10"
-val micrometerVersion = "1.6.5"
-val otelVersion = "1.61.0"
+val micrometerVersion = "1.7.0"
+val kafkaAppender = "0.2.0-RC2"
+val lz4JavaVersion = "1.11.0"
+val otelVersion = "1.63.0"
 val bouncycastleVersion = "1.84"
-val temporalVersion = "1.34.0"
-val protobufJavaVersion = "4.34.1"
-val grpcBomVersion = "1.80.0"
+val temporalVersion = "1.35.0"
+val protobufJavaVersion = "4.35.1"
+val grpcBomVersion = "1.82.0"
 val guavaVersion = "33.6.0-jre"
 val commonsLang3Version = "3.20.0"
 
-val p4paActivitiesVersion = "1.196.3"
+val p4paActivitiesVersion = "1.196.4"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webmvc")
@@ -83,6 +85,10 @@ dependencies {
   }
   implementation("org.apache.commons:commons-lang3:$commonsLang3Version")
   implementation("org.openapitools:jackson-databind-nullable:$openApiToolsVersion")
+  implementation("com.github.danielwegener:logback-kafka-appender:$kafkaAppender") {
+    exclude(group = "org.lz4", module = "lz4-java")
+  }
+  implementation("at.yawk.lz4:lz4-java:$lz4JavaVersion")
   implementation("org.bouncycastle:bcprov-jdk18on:$bouncycastleVersion")
   implementation("it.gov.pagopa.payhub:p4pa-payhub-activities:$p4paActivitiesVersion") {
     exclude(group = "org.glassfish.jaxb", module = "jaxb-core")
@@ -159,6 +165,7 @@ tasks {
       expand(projectInfo)
     }
   }
+  processResources.dependsOn("dependenciesBuild")
 }
 
 tasks.compileJava {
@@ -195,7 +202,8 @@ openApiGenerate {
     mapOf(
       "dateLibrary" to "java8",
       "requestMappingMode" to "api_interface",
-      "useSpringBoot3" to "true",
+      "useSpringBoot4" to "true",
+      "useJackson3" to "true",
       "interfaceOnly" to "true",
       "useTags" to "true",
       "useBeanValidation" to "true",
