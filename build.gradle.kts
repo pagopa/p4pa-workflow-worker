57a49fbdc4ce1225264fd3b38035001a7d92100c
LeonardoPierangeliniNttData <leonardo.pierangelini@nttdata.com>
2026-06-30 15:41:26 +0200

    feat: P4ADEV-4702 sanitizing email attachment filename (#367)

--- build.gradle.kts
+++ build.gradle.kts
@@ -69,7 +69,7 @@ val grpcBomVersion = "1.82.0"
 val guavaVersion = "33.6.0-jre"
 val commonsLang3Version = "3.20.0"
 
-val p4paActivitiesVersion = "1.197.0"
+val p4paActivitiesVersion = "1.201.0"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webmvc")
