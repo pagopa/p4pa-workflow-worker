9ec911de9251df5e1f6ca26fad629588edbe82a0
Leonardo Pierangelini <leonardo.pierangelini@nttdata.com>
2026-08-28 09:42:43 +0200

    P4ADEV-4919-new-updated-campaign-fetch-logic-for-full-counters-recalculation: call new activity for fetching ids of updated campaigns

--- build.gradle.kts
+++ build.gradle.kts
@@ -71,7 +71,7 @@ val grpcBomVersion = "1.82.0"
 val guavaVersion = "33.6.0-jre"
 val commonsLang3Version = "3.20.0"
 
-val p4paActivitiesVersion = "1.203.4"
+val p4paActivitiesVersion = "1.204.0"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webmvc")
