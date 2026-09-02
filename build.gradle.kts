21b16fb6a089e25b8f14b661971aaee5bc40de06
Leonardo Pierangelini <leonardo.pierangelini@nttdata.com>
2026-09-02 10:53:44 +0200

    P4ADEV-4919-new-updated-campaign-fetch-logic-for-full-counters-recalculation: set activity version to 1.204.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -71,7 +71,7 @@ val grpcBomVersion = "1.82.0"
 val guavaVersion = "33.6.0-jre"
 val commonsLang3Version = "3.20.0"
 
-val p4paActivitiesVersion = "1.204.2-SNAPSHOT"
+val p4paActivitiesVersion = "1.204.3"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webmvc")
