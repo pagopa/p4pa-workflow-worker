26c0a6e26f20429eaa8b616185c9732349c8e73b
michele.mazzini <michele.mazzini1996@gmail.com>
2026-09-10 11:44:14 +0200

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -71,7 +71,7 @@ val grpcBomVersion = "1.83.1"
 val guavaVersion = "33.7.0-jre"
 val commonsLang3Version = "3.20.0"
 
-val p4paActivitiesVersion = "1.205.4-SNAPSHOT"
+val p4paActivitiesVersion = "P4ADEV-5002-SNAPSHOT"
 
 // CVE Security dependencies
 val tomcatEmbedCoreVersion = "11.0.25"
